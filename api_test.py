a956eec5728760a47788cde53adba74cd2390222
3303 김민승 <kms20060827@gmail.com>
2024-08-02 15:07:58 +0900

    정류장별 버스 도착 정보 기능

--- api_test.py
+++ api_test.py
@@ -1,4 +1,11 @@
 import requests
+import xmltodict
 
-response = requests.get('http://openapitraffic.daejeon.go.kr/api/rest/busposinfo/getBusPosByRtid?serviceKey=yVuOi%2FAuXdGyvr5hzlwJ37YvM9xxc17Cwdc7%2FR6d2ggfwB7rq0Q%2F9%2BOUQH0VOlODRFlgCizb%2FBKC3M1bnoYALA%3D%3D&busRouteId=30300001')
-print(response.text)
+response = requests.get('http://openapitraffic.daejeon.go.kr/api/rest/arrive/getArrInfoByStopID?serviceKey=1n1TNpJ0Yl3dCAHm96RLhkh%2FnUXR0XqXtOkuoV4zf2u9a2R2dnLGQVUwk2MYyiCEyRHd9Y3u%2BJLMlq6ewbTSYA%3D%3D&BusStopID=8001378')    
+dict_data = xmltodict.parse(response.text)
+l = []
+for i in dict_data['ServiceResult']['msgBody']['itemList']:
+    l.append((i['ROUTE_NO'], i['EXTIME_MIN'], i['MSG_TP']))
+l.sort()
+for i in l:
+    print(i)
