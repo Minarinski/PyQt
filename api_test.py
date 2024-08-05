3138631c13392f2c611e96cff0347477cf8888e6
3303 김민승 <kms20060827@gmail.com>
2024-08-05 10:49:43 +0900

    BusArrivalTimeInfo 중 api 호출 기능 개발

--- api_test.py
+++ api_test.py
@@ -1,7 +1,9 @@
 import requests
 import xmltodict
 
-response = requests.get('http://openapitraffic.daejeon.go.kr/api/rest/arrive/getArrInfoByStopID?serviceKey=1n1TNpJ0Yl3dCAHm96RLhkh%2FnUXR0XqXtOkuoV4zf2u9a2R2dnLGQVUwk2MYyiCEyRHd9Y3u%2BJLMlq6ewbTSYA%3D%3D&BusStopID=8001378')    
+key = 'thWTVZRkftwWDX%2F%2FBffYndD1kH0FA%2B9hrJ4tmX%2FTuFCKo00GOQ4qrPf0Qf5e0C83IRc5yPt%2F%2B6BM6X9n6P%2FSrQ%3D%3D'
+BusStopID = '8001378'
+response = requests.get('http://openapitraffic.daejeon.go.kr/api/rest/arrive/getArrInfoByStopID?serviceKey='+key+'&BusStopID='+BusStopID)    
 dict_data = xmltodict.parse(response.text)
 l = []
 for i in dict_data['ServiceResult']['msgBody']['itemList']:
