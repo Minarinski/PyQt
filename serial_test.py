e4be94ae4550d2b1f2dec42402acfb8a20df4fc4
3303 김민승 <kms20060827@gmail.com>
2024-08-09 14:51:34 +0900

    Serial_test 성공

--- serial_test.py
+++ serial_test.py
@@ -1,17 +1,38 @@
 import serial
-import time
+import re
+#시리얼포트 객체 ser을 생성
+#pc와 스위치 시리얼포트 접속정보
+ser = serial.Serial(
+    port = 'COM5', 
+    baudrate=115200, 
+    parity='N',
+    stopbits=1,
+    bytesize=8,
+    timeout=8
+    )
 
-# 시리얼 포트와 보드레이트를 설정합니다.
-ser = serial.Serial('COM5', 9600)  # 'COM3'을 실제 아두이노가 연결된 포트로 변경하세요.
+#시리얼포트 접속
+ser.isOpen()
 
-try:
-    while True:
-        # "hah" 문자열을 시리얼 포트로 보냅니다.
-        ser.write(b'hah')
-        print("Sent: hah")
-        # 1초 동안 대기합니다.
-        time.sleep(1)
-except KeyboardInterrupt:
-    # 프로그램이 종료될 때 시리얼 포트를 닫습니다.
-    ser.close()
-    print("Serial port closed")
+#시리얼포트 번호 출력
+print(ser.name)
+
+# 정규식 패턴
+pattern = re.compile('^0x02.*0x03$')
+        
+while True:
+    if ser.in_waiting > 0:  # 읽을 데이터가 있는지 확인
+        data = ser.readline().decode('utf-8').rstrip() # 데이터를 읽고 디코딩
+        # 정규식 매칭
+        if pattern.match(data):
+            dataSplit = data[4:-4].split(',')
+            if dataSplit[1] == '1':
+                print(f"{dataSplit[0]}번 버스 요청")
+            elif dataSplit[1] == '2':
+                print(f"{dataSplit[0]}번 버스 헬프콜")
+            else:
+                print(f"{dataSplit[0]}번 버스 요청 취소")
+
+
+# 사용이 끝나면 시리얼 포트를 닫습니다.
+ser.close()
